37239e0f6fa15784041d89bc58251e997e032ea6
Bruno Marazzi <brunomarazzidev@gmail.com>
2023-12-01 14:05:52 -0300

    new version

--- app.py
+++ app.py
@@ -1,50 +1,62 @@
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-app = Flask(__name__, template_folder='templates')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://localhost/FerramentasDB?driver=ODBC+Driver+17+for+SQL+Server&auth=Windows'
-
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ferramentas.db'
 db = SQLAlchemy(app)
 
-class Ferramentas(db.Model):
+class Ferramenta(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(50), nullable=False)
-    manufacturer = db.Column(db.String(50), nullable=False)
-    acquisitionDate = db.Column(db.Date, nullable=False)
-    lastCalibrationDate = db.Column(db.Date, nullable=False)
-    nextCalibrationDate = db.Column(db.Date, nullable=False)
-    warranty = db.Column(db.String(50), nullable=False)
-
-with app.app_context():
-    db.create_all()
+    tipo = db.Column(db.String(100), nullable=False)
+    fabricante = db.Column(db.String(100), nullable=False)
+    data_aquisicao = db.Column(db.Date, nullable=False)
+    data_ultima_calibracao = db.Column(db.Date, nullable=False)
+    data_proxima_calibracao = db.Column(db.Date, nullable=False)
+    garantia = db.Column(db.String(50), nullable=False)
 
 @app.route('/')
 def index():
-    tools = Ferramentas.query.all()
-    return render_template('/index.html', tools=tools)
+    ferramentas = Ferramenta.query.all()
+    return render_template('index.html', ferramentas=ferramentas)
 
 @app.route('/add_tool', methods=['POST'])
 def add_tool():
-    new_tool = Ferramentas(id='ID', type='Ferramenta Nova', manufacturer='Fabricante Novo', acquisitionDate='Data de Aquisicao', lastCalibrationDate='Data da ultima calibracao', nextCalibrationDate='Data da proxima calibracao', warranty='Garantia')
+    tipo = request.form['tipo']
+    fabricante = request.form['fabricante']
+    data_aquisicao = datetime.strptime(request.form['data_aquisicao'], '%Y-%m-%d')
+    data_ultima_calibracao = datetime.strptime(request.form['data_ultima_calibracao'], '%Y-%m-%d')
+    data_proxima_calibracao = datetime.strptime(request.form['data_proxima_calibracao'], '%Y-%m-%d')
+    garantia = request.form['garantia']
 
-    db.session.add(new_tool)
+    nova_ferramenta = Ferramenta(
+        tipo=tipo,
+        fabricante=fabricante,
+        data_aquisicao=data_aquisicao,
+        data_ultima_calibracao=data_ultima_calibracao,
+        data_proxima_calibracao=data_proxima_calibracao,
+        garantia=garantia
+    )
+
+    db.session.add(nova_ferramenta)
     db.session.commit()
 
     return redirect(url_for('index'))
 
+@app.route('/consult_tool/<int:id>')
+def consult_tool(id):
+    ferramenta = Ferramenta.query.get(id)
+    return render_template('consult_tool.html', ferramenta=ferramenta)
+
+@app.route('/delete_tool/<int:id>')
+def delete_tool(id):
+    ferramenta = Ferramenta.query.get(id)
+    db.session.delete(ferramenta)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    
     app.run(debug=True)
-
-import logging
-logging.basicConfig()
-logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
-
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.event import listens_for
-
-db = SQLAlchemy(app)
-
-@listens_for(Ferramentas, 'after_insert')
-def receive_after_insert(mapper, connection, target):
-    app.logger.info('Ferramenta inserida no banco de dados!')
